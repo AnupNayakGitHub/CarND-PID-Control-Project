19985d760ecb5c96cad6d814fe5aefa986c25d41
SDC <sdc@SmritiN-MBP-3.home>
2017-12-14 12:14:21 -0500

    updated

--- src/PID.cpp
+++ src/PID.cpp
@@ -3,10 +3,6 @@
 
 using namespace std;
 
-/*
-* TODO: Complete the PID class.
-*/
-
 PID::PID():p_error(0.0), i_error(0.0), d_error(0.0),
            Kp(0.0), Ki(0.0), Kd(0.0), tune(false), state(TUNED) {}
 
@@ -25,7 +21,6 @@ void PID::Tune(){
 void PID::Tune(double tol, unsigned int steps_per_adj) {
   this->tune = true;
   state = STABILIZE;
-  //twdlr_st = 0;
   twdlr.insert(Kp, 1);
   twdlr.insert(Kd, 1);
   twdlr.set_steps_per_iteration(steps_per_adj);
@@ -33,7 +28,6 @@ void PID::Tune(double tol, unsigned int steps_per_adj) {
 }
 
 bool PID::IsTuning() {
-  //return (tune && ((!twdlr.isStable()) || (twdlr_st != 3)));
   return (tune);
 }
 unsigned int PID::TwiddlerAdjustments() {
@@ -44,7 +38,6 @@ void PID::ResetTwiddlerAdjustments() {
 }
 
 bool PID::NeedsReset() {
-  //return (twdlr_st == 0);
   return (state == STABILIZE);
 }
 
